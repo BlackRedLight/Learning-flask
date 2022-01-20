550c2e4fc3bcb32b0f4bf1f65cace8189adb31db
BlackRedLight <blackredlight@BRL.Laptop>
2022-01-20 23:22:30 +0500

    Added site template

--- main.py
+++ main.py
@@ -5,4 +5,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-	return render_template('home.html')
+	return render_template('index.html')
+
+@app.route('/works')
+def works_page():
+	return render_template('works.html')
+
+@app.route('/about')
+def about_page():
+	return render_template('about.html')
+
+@app.route('/contact')
+def contact_page():
+	return render_template('contact.html')
+
+@app.route('/components')
+def components_page():
+	return render_template('components.html')
+
+@app.route('/work')
+def work_page():
+	return render_template('components.html')
